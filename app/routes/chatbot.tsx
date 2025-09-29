cc771695d7efb4fbb8f4d1bd133ea90abccf798f
Jahnik <jahniksuriya@gmail.com>
2025-09-29 12:15:42 -0400

    got drizzle working

---
+++ app/routes/chatbot.tsx
@@ -0,0 +1,7 @@
+import type { Route } from "./+types/home";
+import { Welcome } from "../welcome/welcome";
+
+
+export default function Chatbot() {
+    return <div>Hello This is where my chatbot will go!</div>;
+}
