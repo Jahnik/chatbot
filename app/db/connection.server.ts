060095bdca0cc69d4fce70a436f0cea006602f77
Jahnik <jahniksuriya@gmail.com>
2025-10-02 17:07:11 -0400

    changed db connection logic to use transaction pooler, bc supabase doesn 't allow IPv4 connections directly, note that the .env variable had to be changed

--- app/db/connection.server.ts
+++ app/db/connection.server.ts
@@ -2,5 +2,5 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
 if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL not found!")
-const client = postgres(process.env.DATABASE_URL)
+const client = postgres(process.env.DATABASE_URL, { prepare: false })
 export const db = drizzle(client);
